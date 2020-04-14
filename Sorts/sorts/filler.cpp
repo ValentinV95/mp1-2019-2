27d4e1429de66ebeb8d33f2c685200b9205410c9
Daniil Kiselev <kiselev.danil201313@gmail.com>
2020-04-14 23:25:37 +0300

    folder fix

--- Sorts/sorts/filler.cpp
+++
@@ -1,8 +0,0 @@
-#include "sorts.h"
-
-void filler(double* a, int n) {
-	for (int i = 0; i < n; i++)
-	{
-		a[i] = (double)rand()/RAND_MAX*(500000);
-	}
-}
--- Sorts/sorts/swapf.cpp
+++
@@ -1,7 +0,0 @@
-#include "sorts.h"
-
-void swapf(double& a, double& b) {
-	double tmp = a;
-	a = b;
-	b = tmp;
-}
