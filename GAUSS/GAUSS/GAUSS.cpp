af50875e917d8f6578382e2f415e1e04f3872eb2
Nedoshivin Danil <niedoshivin2@mail.ru>
2020-05-26 12:56:31 +0300

    Check it pls

---
+++ GAUSS/GAUSS/GAUSS.cpp
@@ -0,0 +1,18 @@
+﻿#include "classes.h"
+
+int main()
+{
+    int s;
+    cout << "vvedite razmer vectora" << endl;
+    cin >> s;
+    Vector <double> mVector(s);
+    slau<double>m(s);
+    m.RandM();
+    cout << "pervonachalnaya matrica" << endl;
+    m.printM();
+    mVector = m.gauss();
+    cout << "Treugolnaya matrica:" << endl;
+    m.printM();
+    cout << "Recheniye sistemi:" << endl;
+    mVector.printV();
+}
