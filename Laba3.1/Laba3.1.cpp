4fcc2ca5c289d5033613107108542ee2751f8c69
Mascart <Mr.Mascart.kl@yandex.ru>
2020-05-26 22:14:23 +0300

    Matrix

---
+++ Laba3.1/Laba3.1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "Vector.h"
+#include "Matrix.h"
+#include "Gauss.h"
+using namespace std;
+
+int main()
+{
+	int size;
+	cout << "Input matrix size:" << endl;
+	cin >> size;
+	Gauss<double>slae(size);
+	Vector<double>sol(size);
+	slae.InputMat();
+	cout << "Original matrix:" << endl;
+	slae.s_print();
+
+	sol = slae.gauss();
+	cout << "Triangular matrix:" << endl;
+	slae.s_print();
+
+	cout << "Solution vector:" << endl;
+	sol.v_print();
+
+	
+	
+}
