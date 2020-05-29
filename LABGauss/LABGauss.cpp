2397c76a03501b8dc8f8a3f8e5b1810528caa956
Nik Timakov <56776499+KurazhAllDay@users.noreply.github.com>
2020-05-29 15:51:02 +0300

    Add files via upload

---
+++ LABGauss/LABGauss.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "Vector.h"
+#include "Matrix.h"
+#include "Linear_Sistem.h"
+
+using namespace std;
+
+int main()
+{
+    int SIZE; 
+    cout << "Gauss method of solving systems of linear equations." << endl;
+    cout << "Enter vector size: " << endl;  
+    cin >> SIZE;
+
+    Vector <double> matrixVector(SIZE);
+    Linear_System<double>matrix(SIZE);
+
+    matrix.Fill();  
+    cout << "Initial Matrix: " << endl;  
+    matrix.printMatrix();
+
+    matrixVector = matrix.Gauss(); 
+
+    cout << "Triangular Matrix: " << endl;
+    matrix.printMatrix();
+
+    cout << "System solution: " << endl;
+    matrixVector.printVector();
+
+    matrix.Check();
+
+}
+
+
