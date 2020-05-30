ebf273af7770841d43f11e6d6a275e3248233f49
Nik Timakov <56776499+KurazhAllDay@users.noreply.github.com>
2020-05-30 18:28:44 +0300

    Chto-to ispravil

--- LABGauss/LABGauss.cpp
+++ LABGauss/LABGauss.cpp
@@ -5,30 +5,48 @@
 
 using namespace std;
 
-int main()
-{
-    int SIZE; 
-    cout << "Gauss method of solving systems of linear equations." << endl;
-    cout << "Enter vector size: " << endl;  
-    cin >> SIZE;
+int SIZE;
+	cout << "Gauss method of solving systems of linear equations." << endl;
+	cout << "Enter vector size: " << endl;
+	cin >> SIZE;
 
-    Vector <double> matrixVector(SIZE);
-    Linear_System<double>matrix(SIZE);
+	Vector <double> matrixVector(SIZE);
+	Linear_System<double>matrix(SIZE);
 
-    matrix.Fill();  
-    cout << "Initial Matrix: " << endl;  
-    matrix.printMatrix();
+	matrix.FillMatrix();
+	
+	matrix.FillRight();
+	cout << "Initial Matrix: " << endl;
+	matrix.printMatrix();
 
-    matrixVector = matrix.Gauss(); 
+	matrixVector = matrix.Gauss();
+	cout << "Triangular Matrix: " << endl;
+	matrix.printMatrix();
+	cout << "System solution: " << endl;
+	matrixVector.printVector();
+	matrix.Check();
+	int raz;
+	cout << "Matriza tazhe, right chast' drygaya!" << endl;
+	cout << "Vvedite how many raz right chast' will be drygoy: ";
+	cin >> raz;
+	cout << endl;
+	for (int i = 0; i < raz; i++)
+	{
+		cout << "New right values: " << endl;
+		matrix.FillRight();
+		cout << "Initial Matrix: " << endl;
+		matrix.printMatrix();
 
-    cout << "Triangular Matrix: " << endl;
-    matrix.printMatrix();
+		matrixVector = matrix.Gauss();
+		cout << "Triangular Matrix: " << endl;
+		matrix.printMatrix();
 
-    cout << "System solution: " << endl;
-    matrixVector.printVector();
+		cout << "System solution: " << endl;
+		matrixVector.printVector();
+		matrix.Check();
+	}
 
-    matrix.Check();
+	
 
 }
-
 
